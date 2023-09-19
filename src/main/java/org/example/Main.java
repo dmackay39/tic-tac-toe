655ab10e596284afb33093c351314c48d332891a
Daniel MacKay <84335791+dmackay39@users.noreply.github.com>
2023-09-19 15:29:35 +0100

    Updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         GameLogic gameLogic = new GameLogic();
         gameLogic.playGame();
-        //some meaningless change to get github to work
     }
 }
