489efb71f7935efcd0fe178277b6c1c4304363e2
Daniel MacKay <84335791+dmackay39@users.noreply.github.com>
2023-09-19 14:35:36 +0100

    Something wrong with github language display

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         GameLogic gameLogic = new GameLogic();
         gameLogic.playGame();
+        //some meaningless change to get github to work
     }
 }
